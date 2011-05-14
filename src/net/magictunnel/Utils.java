5f428e1ff5adf403b0e04befec50eb7cf5ce6a14
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-05-15 00:33:43 +0200

    Added logo to about box

--- src/net/magictunnel/Utils.java
+++ src/net/magictunnel/Utils.java
@@ -2,10 +2,17 @@ package net.magictunnel;
 
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.net.Uri;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.TextView;
 
 public class Utils {
@@ -36,7 +43,20 @@ public class Utils {
 		dialog.setContentView(R.layout.aboutbox);
 		TextView tv = (TextView)dialog.findViewById(R.id.about_url);
 		tv.setMovementMethod(LinkMovementMethod.getInstance());
-		tv.setText(Html.fromHtml(c.getString(R.string.about_url)));
+		tv.setOnClickListener(new OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				String url = v.getContext().getString(R.string.about_url);
+				Intent intent = new Intent("android.intent.action.VIEW", Uri.parse(url));
+
+				try {
+				 v.getContext().startActivity(intent);
+				} catch (ActivityNotFoundException ex) {
+				 // do something about the exception, or not ...
+				}
+
+			}
+		});
 		dialog.show();
 	}
 
