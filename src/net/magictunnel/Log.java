3b0aea7ec3071500163eff93366f049f44574139
Vitaly Chipounov <vitaly.chipounov@epfl.ch>
2011-05-28 18:37:45 +0200

    Copy log to clipboard (closes #19)

--- src/net/magictunnel/Log.java
+++ src/net/magictunnel/Log.java
@@ -2,12 +2,18 @@ package net.magictunnel;
 
 import net.magictunnel.core.Iodine;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.ClipboardManager;
 import android.text.method.ScrollingMovementMethod;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 
 public class Log extends Activity {
+	private static final int MENU_COPYLOG = Menu.FIRST;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -27,4 +33,30 @@ public class Log extends Activity {
 			textview.setText(iod.getLog().toString());
 		}
     }
+
+    @Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		menu.add(0, MENU_COPYLOG, 0, R.string.log_copy).setIcon(
+				android.R.drawable.ic_menu_share);
+		return true;
+	}
+
+    @Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+
+		switch (item.getItemId()) {
+		case MENU_COPYLOG:
+			ClipboardManager clipboard = (ClipboardManager)getSystemService(CLIPBOARD_SERVICE);
+	        MagicTunnel mt = ((MagicTunnel) getApplication());
+			Iodine iod = mt.getIodine();
+			clipboard.setText(iod.getLog().toString());
+			break;
+
+		default:
+			return false;
+		}
+
+		return true;
+	}
 }
